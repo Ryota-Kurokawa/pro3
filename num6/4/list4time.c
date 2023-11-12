b7b9035cd02690f8d3446b6da778e8ef42bf958c
“Ryota <kurokwa.ryota.1124@gmail.com>
2023-11-12 20:20:37 +0900

    num6

--- num6/4/list4time.c
+++ num6/4/list4time.c
@@ -8,8 +8,9 @@
 int main(void)
 {
   clock_t clk_start, clk_end;
+
   list_node_t *head_p = create_node(0); /* ヘッダ (ダミー) */
-  int n, m = 0;
+  int n = 0;
   // (0)
   scanf("%d", &n);
   // (1)
@@ -17,22 +18,25 @@ int main(void)
   myrand_init();
   for (int i = 0; i < n; i++)
   {
-    m = myrand();
-    list_insert_uniq(head_p, m);
+    list_insert_uniq(head_p, myrand());
   }
+  clk_end = clock();
+  printf("cpu = %f [sec]\n", (double)(clk_end - clk_start) / CLOCKS_PER_SEC);
   // (2)
+  clk_start = clock();
   myrand_init();
 
   for (int i = 0; i < n; i++)
   {
-    m = myrand();
-    list_find(head_p, m);
+    list_find(head_p, myrand());
   }
+  clk_end = clock();
+  printf("cpu = %f [sec]\n", (double)(clk_end - clk_start) / CLOCKS_PER_SEC);
   // (3)
+  clk_start = clock();
   for (int i = 0; i < n; i++)
   {
-    m = myrand();
-    list_find(head_p, m);
+    list_find(head_p, myrand());
   }
   clk_end = clock();
   printf("cpu = %f [sec]\n", (double)(clk_end - clk_start) / CLOCKS_PER_SEC);
