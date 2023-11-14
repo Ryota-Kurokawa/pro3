76dd4de4de0cd668a446edf25df685b44f08bfa6
“Ryota <kurokwa.ryota.1124@gmail.com>
2023-11-14 15:44:37 +0900

    7-1

---
+++ num7/1/swapmin.c
@@ -0,0 +1,14 @@
+void swapmin(int n, double *a, int k)
+{
+  int min = k;
+  for (int i = k; i < n; i++)
+  {
+    if (a[i] < a[min])
+    {
+      min = i;
+    }
+  }
+  double tmp = a[k];
+  a[k] = a[min];
+  a[min] = tmp;
+}
