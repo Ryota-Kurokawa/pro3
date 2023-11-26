ade93fde1da7366e09fea9a198b4a06b4311020f
“Ryota <kurokwa.ryota.1124@gmail.com>
2023-11-26 13:48:29 +0900

    仮

---
+++ num8/5/iata_cpu1.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <time.h>
+#include "iata_db.h"
+
+int main(void)
+{
+  db_t db;
+  char key[KEY_LEN + 1];
+  char *data;
+
+  db_init(&db);
+  db_load(&db);
+
+  /* 空港コードを入力するとデータを出力; EOFまで繰り返し実行 */
+  for (;;)
+  {
+    clock_t clk_start, clk_end;
+    clk_start = clock();
+    fprintf(stderr, "key = ");
+    if (scanf(KEY_FMT, key) == EOF)
+    {
+      break;
+    }
+    for (int i = 0; i < 100000; i++)
+    {
+      data = db_binary_search(&db, key);
+    }
+    clk_end = clock();
+    fprintf(stderr, "cpu = %f\n", (double)(clk_end - clk_start) / CLOCKS_PER_SEC);
+    if (data != NULL)
+    {
+      printf("%s => %s\n", key, data);
+    }else
+    {
+      printf("NO RECORD\n");
+    }
+    
+  }
+
+  db_delete(&db);
+  return 0;
+}
